ca74ed63e70b392f982d4f1e2495b57800637831
M1XU <mikko.hamalainen@maestro.fi>
2017-10-19 11:13:43 +0300

    ...

---
+++ conditional-statements/Funktiot/ConsoleApplication1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditional_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // program logic
+            int n;
+            while (true)
+            {
+                Console.Write("Tähtien lukumäärä: ");
+                n = int.Parse(Console.ReadLine());
+                if (n < 0)
+                {
+                    Console.WriteLine("Luku {0} ei ole sallittuluku", n);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string asterisks = getNAsterisks(n);
+            Console.WriteLine(asterisks);
+            Console.ReadKey();
+        }
+        static string getNAsterisks(int n)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                res += '*';
+            }
+            return res;
+        }
+    }
+}
+
+
+
+
+
+
+
+
