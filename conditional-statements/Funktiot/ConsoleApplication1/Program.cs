3db71de5967f62637d6d0b911f53f3ea10ef775f
M1XU <mikko.hamalainen@maestro.fi>
2017-10-25 11:46:15 +0300

    ...

--- conditional-statements/Funktiot/ConsoleApplication1/Program.cs
+++ conditional-statements/Funktiot/ConsoleApplication1/Program.cs
@@ -11,32 +11,49 @@ namespace conditional_statement
         static void Main(string[] args)
         {
             // program logic
-            int n;
-            while (true)
-            {
-                Console.Write("Tähtien lukumäärä: ");
-                n = int.Parse(Console.ReadLine());
-                if (n < 0)
-                {
-                    Console.WriteLine("Luku {0} ei ole sallittuluku", n);
-                }
-                else
-                {
-                    break;
-                }
-            }
-            string asterisks = getNAsterisks(n);
-            Console.WriteLine(asterisks);
+            int a;
+            int b;
+            // int m;
+            // while (true)
+            //{
+                Console.WriteLine("Anna kaksi lukua ja ohjelma palauttaa sinulle luvuista pienemmän");
+                Console.Write("Anna ensimmäinen luku: ");
+                a = int.Parse(Console.ReadLine());
+                Console.Write("Anna toinen luku: ");
+                b = int.Parse(Console.ReadLine());
+                // if (n < 0)
+                // {
+                //     Console.WriteLine("Luku {0} ei ole sallittuluku", n);
+                // }
+                // else
+                // {
+                //     break;
+                // }
+            //}
+            string res = getMinimum(a,b);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+            int resb = getMinimumInt(a, b);
+            Console.WriteLine(resb);
             Console.ReadKey();
         }
-        static string getNAsterisks(int n)
-        {
-            string res = string.Empty;
-            for (int i = 0; i < n; i++)
+            static string getMinimum(int x, int y)
             {
-                res += '*';
+                string res = string.Empty;
+                if (x < y)
+                    res = x.ToString();
+                else
+                    res = y.ToString();
+                return res;
             }
-            return res;
+        static int getMinimumInt(int x, int y)
+        {
+            return x < y ? x : y;
+            //if (x < y)
+            //    return x;
+            //else                
+            //    return y;
         }
     }
 }
---
+++ conditional-statements/Funktiot/Funktiot3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditional_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // program logic
+            Console.WriteLine(reqNumbers(10));
+            Console.ReadKey();
+        }
+        static int reqNumbers(int n)
+        {
+            Console.WriteLine($"Syötä {n} lukua : ");
+            string res = string.Empty;
+            int max = 0;
+            int jarjestys = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"{i}. ");
+                int a = int.Parse(Console.ReadLine());
+                if (a < 0)
+                {
+                    Console.WriteLine($"Luku {a} ei ole sallittu luku");
+                    i--;
+                }
+                else
+                {
+//                    Console.WriteLine($"{i}. {a}");
+                    res += $"{a} ,";
+                }
+
+                if (a > max) max = a;
+                if (a == max) jarjestys = i;  
+                
+            }
+            Console.WriteLine(res);
+            Console.WriteLine($"Suurin luku on {max}, ja se oli {jarjestys}. syöttämäsi luku.");
+            return max;
+        }
+    }
+}
